e412faee9b24142820d4a294e3feee98e3b3fc5f
Gede Lumbung <gedesumawijaya@gmail.com>
2014-09-17 18:43:08 +0800

    initial

---
+++ src/alert/detail/dlmbg/AlertDetailToastActivity.java
@@ -0,0 +1,35 @@
+package alert.detail.dlmbg;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class AlertDetailToastActivity extends ListActivity {
+	String[] wisata = { "Alun-Alun Kota",
+			"Kawah Ijen",
+			"Pantai Watu Dodol",
+			"Teluk Hijau",
+			"Pantai Sukamade",
+			"Air Terjun Lider", 
+			"Pantai Bedul", 
+			"Alas Purwa", 
+			"Pantai Plengkung",
+			"Pulau Merah"};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, wisata));
+	}
+
+	public void onListItemClick(ListView parent, View v, int urutan, long id) {
+		Object detail = this.getListAdapter().getItem(urutan);
+		String tampil = detail.toString();
+		Toast.makeText(this, "Wisata Banyuwangi : " + tampil, Toast.LENGTH_SHORT).show();
+
+	}
+}
